2e28c5d4e072dcd1229a631901bf4ab545c33353
kim <k.c.duong@st.hanze.nl>
2019-11-21 13:37:31 +0100

    Created python script that's going to compare the DB search outputs. For now just docstrings

---
+++ DbSearchComparison.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3.6
+import sys
+import datetime
+
+"""
+Short script to compare the PEAKS psm search output to find overlap and distinction between the matched peptides.
+"""
+
+
+def extract_peptide_column():
+    """extracts peptide column and removes unnecessary information, such as the modification between parentheses"""
+
+
+def parse_genemark_search():
+    """Parses the genemark file to extract the peptide column and matching ORF"""
+
+
+
+def parse_transdecoder_search():
+    """Parses the transdecoder file to extract the peptide column and matching ORFs"""
+
+
+parse_genemark_search()
+
+
+def main():
+    print("Hello, World!")
