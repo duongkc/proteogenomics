aab83d879a859bd86ed3e28b322104652ca21fd7
kim <k.c.duong@st.hanze.nl>
2019-11-25 14:27:53 +0100

    Add peptide line cleaning method
    It removes the period symbols and info inside parentheses (modification)
    from the peptide strings.

--- DbSearchComparison.py
+++ DbSearchComparison.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3.6
 import sys
+import re
 import datetime
 
 """
@@ -7,18 +8,26 @@ Short script to compare the PEAKS psm search output to find overlap and distinct
 """
 
 
-def extract_peptide_column():
-    """extracts peptide column and removes unnecessary information, such as the modification between parentheses"""
+def strip_peptide_col(peptide_column):
+    """strips peptide column by removing unnecessary information and returns the peptide"""
+    sample_line = "K.ELC(+57.02)EQ(+.98)EC(+57.02)EWEEITITGSDGSTR.V"
+    no_parentheses_pep = re.sub(r'\([^()]*\)', '', peptide_column)
+    stripped_pep = no_parentheses_pep.replace('.', '')
+    return stripped_pep
+
 
 
 def parse_genemark_search():
-    """Parses the genemark file to extract the peptide column and matching ORF"""
-    gmfile = "data/genemark_dbsearch_psm.csv"  # Change to non static later
+    """Parses the genemark file to extract the peptide column and matching ORF accession"""
+    gmfile = "data/propep_g.csv"  # Change to non static later
     with open(gmfile, "r") as f:
         next(f)
         for line in f:
             columns = line.split(",")
-            print(columns[0])
+            accession = columns[2]
+            peptide_col = columns[3]
+            peptide = strip_peptide_col(peptide_col)
+            print(peptide)
 
 
 def parse_transdecoder_search():
