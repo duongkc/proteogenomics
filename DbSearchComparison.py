cb323424b30eadbcbbf95afe766dc4a1e84b1c07
kim <k.c.duong@st.hanze.nl>
2019-11-29 11:51:51 +0100

    Remove the temporary file outputs
    Functions now use pandas dataframes that get read into memory once instead a temporary printed file that wouldn't get deleted yet

--- DbSearchComparison.py
+++ DbSearchComparison.py
@@ -19,37 +19,18 @@ def clean_peptide_col(peptide_column):
 
 def extract_csv_data(input_file, db_type):
     """Parses the PEAKS protein-peptide csv file to extract the peptides and matching ORF accessions"""
-    # And write them to a new temporary file, (should be split into sub methods tbh
-    if db_type == "genemark":
-        temp_output = "output/temp_gm_output.csv"
-    else:
-        temp_output = "output/temp_td_output.csv"
-
-    # with open(temp_output, "w+") as temp_peptide_output:
-    #     temp_peptide_output.write("Protein Accession,Peptide\n")
-    #     with open(input_file, "r") as f:
-    #         next(f)
-    #         for line in f:
-    #             columns = line.split(",")
-    #             accession = columns[2]
-    #             peptide_col = columns[3]
-    #             peptide = clean_peptide_col(peptide_col)
-    #             new_line = accession + "," + peptide + "\n"
-    #             temp_peptide_output.write(new_line)
     csv_data = pandas.read_csv(input_file, header='infer', delimiter=',')
     for i, row in csv_data.iterrows():
         raw_peptide = csv_data.at[i, 'Peptide']
         csv_data.at[i, 'Peptide'] = clean_peptide_col(raw_peptide)
-    with open(temp_output, "w+") as temp_peptide_output:
-        csv_data[['Protein Accession', 'Peptide']].to_csv(temp_peptide_output, sep=',', mode='w', index=False)
+    csv_data = csv_data.drop_duplicates(subset=['Protein Accession', 'Peptide'], keep='first')
+    return csv_data
 
 
-def find_distinct_peptides(transdecoder_file, genemark_file):
+def find_distinct_peptides(transdecoder_data, genemark_data):
     """Filters the CSV files so only distinct peptides remain"""
     with open("output/distinct_td.csv", "w+") as distinct_transdecoder, \
             open("output/distinct_gm.csv", "w+") as distinct_genemark:
-        transdecoder_data = pandas.read_csv(transdecoder_file, header='infer').drop_duplicates()
-        genemark_data = pandas.read_csv(genemark_file, header='infer').drop_duplicates()
         td_merged = pandas.merge(transdecoder_data, genemark_data, on='Peptide', how='left', indicator=True) \
             .query("_merge == 'left_only'")
         gm_merged = pandas.merge(transdecoder_data, genemark_data, on='Peptide', how='right', indicator=True) \
@@ -60,11 +41,10 @@ def find_distinct_peptides(transdecoder_file, genemark_file):
             .to_csv(distinct_genemark, sep=',', mode='w', index=False, header=['Protein Accession', 'Peptide'])
 
 
-extract_csv_data("data/propep_g.csv", "genemark")
+genemark_data = extract_csv_data("data/propep_g.csv", "genemark")
+transdecoder_data = extract_csv_data("data/propep_t.csv", "transdecoder")
 
-
-# extract_csv_data("data/propep_t.csv", "transdecoder")
-# find_distinct_peptides("output/temp_td_output.csv", "output/temp_gm_output.csv")
+find_distinct_peptides(transdecoder_data, genemark_data)
 
 
 def main():
