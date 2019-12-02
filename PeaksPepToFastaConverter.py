65f72663a2e4c0bdc7489ea977cbdb937644126d
kim <k.c.duong@st.hanze.nl>
2019-12-02 15:21:03 +0100

    Create output fasta writing function
    Uses the unique accessions from extract function. Still unfinished.

--- PeaksPepToFastaConverter.py
+++ PeaksPepToFastaConverter.py
@@ -6,8 +6,21 @@ import pandas
 import getopt
 import os
 
+
+def find_pep_from_accession(accession):
+    """retrieves pep sequence from the full pep fasta file"""
+    output = "distinct_pep_fasta/{}_distinct_pep.fasta".format("test")
+    with open(output, "w+") as new_fasta:
+        print("Hello, World!")
+
+
 def extract_comparison_csv_data():
     """Reads the comparison csv data as dataframe"""
     input_file = "comparison_output/test1_distinct_gm.csv"
     csv_data = pandas.read_csv(input_file, header='infer', delimiter=',')
-    
+    unique_accessions = csv_data[['Protein Accession']].drop_duplicates(keep='first')
+
+    return unique_accessions
+
+
+accessions = extract_comparison_csv_data()
