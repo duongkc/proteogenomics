c88a8dd736d9fd50000ea2987be846b2c2f25455
kim <k.c.duong@st.hanze.nl>
2019-12-03 11:30:47 +0100

    Add docstring and command line options

--- PeaksPepToFastaConverter.py
+++ PeaksPepToFastaConverter.py
@@ -6,6 +6,10 @@ import pandas
 import getopt
 import os
 
+"""
+usage: PeaksPepToFastaConverter.py -c <distinct peptide csv> -f <peptide fasta> -p <output prefix>
+"""
+
 
 def find_pep_from_accession(old_fasta, accession):
     """retrieves pep sequence from the full pep fasta file"""
@@ -20,33 +24,55 @@ def find_pep_from_accession(old_fasta, accession):
     return sequence
 
 
-def extract_comparison_csv_data():
+def extract_comparison_csv_data(input_file):
     """Reads the comparison csv data as dataframe"""
-    input_file = "comparison_output/fake_sample.csv"
     csv_data = pandas.read_csv(input_file, header='infer', delimiter=',')
     unique_accessions = csv_data[['Protein Accession']].drop_duplicates(keep='first')
 
     return unique_accessions
 
 
-def main():
+def main(argv):
+    print(' '.join(argv))
+    print("started at: " + datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
+    input_csv = ''
+    old_pep_fasta = ''
+    output_prefix = ''
+
+    try:
+        opts, args = getopt.getopt(argv[1:], 'c:f:p:', ['csv=', 'fasta=', 'prefix='])
+    except getopt.GetoptError:
+        print("usage: PeaksPepToFastaConverter.py -c <distinct peptide csv> -f <peptide fasta> -p <output prefix>")
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ('-c', '--csv'):
+            input_csv = arg
+        elif opt in ('-f', '--fasta'):
+            old_pep_fasta = arg
+        elif opt in ('-p', '--prefix'):
+            output_prefix = arg
+        else:
+            print("usage: PeaksPepToFastaConverter.py -c <distinct peptide csv> -f <peptide fasta> -p <output prefix>")
+            sys.exit(2)
+
     try:
         os.makedirs("distinct_pep_fasta")
     except FileExistsError:
         pass
 
-    accessions = extract_comparison_csv_data()
-    output = "distinct_pep_fasta/{}_distinct_pep.fasta".format("test")
-    old_pep_fasta = "output/Trinity.fasta.genemark.pep"
+    accessions = extract_comparison_csv_data(input_csv)
+    output = "distinct_pep_fasta/{}_distinct_pep.fasta".format(output_prefix)
     with open(output, "w+") as new_fasta, open(old_pep_fasta, "r") as old_fasta:
         for i, row in accessions.iterrows():
             accession = row['Protein Accession']
             accession_seq = find_pep_from_accession(old_fasta, accession)
             new_fasta.write(">" + accession + "\n")
             new_fasta.write(accession_seq)
+    print("finished at: " + datetime.datetime.now().strftime("%m/%d/%Y, %H:%M:%S"))
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
 
 
