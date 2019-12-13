da7eec372dffd683b0ebe07abcffad5e1c29fdc7
kim <k.c.duong@st.hanze.nl>
2019-12-13 10:36:16 +0100

    Add new Python module for future use
    This module is for comparing the PEAKS result to an existing database.

---
+++ unknown_peptide_seeker.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+"""
+A module that checks how many peptides from the PEAKS protein-peptide match results can be found in
+existing protein sequence databases and filters the unknown peptides to a separate file.
+"""
