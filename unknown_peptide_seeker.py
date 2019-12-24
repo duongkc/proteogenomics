2285602713c8cbe697a9891e5be9bdaf150be011
KC <k.c.duong@st.hanze.nl>
2019-12-24 17:32:12 +0100

    Use getopt and add other code for actual usage of the module
    Now takes in csv file, database fasta and preferred prefix
    from the terminal when running this python file.
    Also reversed the flag list so only unrecognized peptides
    will be written to the new file

--- unknown_peptide_seeker.py
+++ unknown_peptide_seeker.py
@@ -2,17 +2,19 @@
 """
 A module that checks how many peptides from the PEAKS protein-peptide match results can be found in
 existing protein sequence databases and filters the unknown peptides to a separate file.
+
+Usage: unknown_peptide_seeker.py -c <PEAKS csv file> -d <protein database fasta> -p <output prefix>
 """
 import datetime
-import gzip
+import getopt
 import multiprocessing as mp
-from os import cpu_count
+import os
 import re
 import sys
 
+import numpy as np
 import pandas
 from Bio import SeqIO
-import numpy as np
 
 
 def clean_peptide_col(peptide_column):
@@ -48,7 +50,7 @@ def search_peptide_db(arguments):
 
     with open(database_file, "r") as database:
         count = 1
-        flag_list = [1] * len(peptide_data.index)  # REVERSE THIS
+        flag_list = [1] * len(peptide_data.index)
         for record in SeqIO.parse(database, "fasta"):
             if (count - offset) % n == 0:
                 for i, row in peptide_data.iterrows():
@@ -69,35 +71,65 @@ def merge_flags(flags):
     return merged_flag_list
 
 
-def write_unknown_peptide_data(peptide_data, merged_flag_list):
-    output = "output/test_unknowns.csv"
+def write_unknown_peptide_data(peptide_data, merged_flag_list, prefix):
+    output = "output/unknown_peptides/{}_unknown_peptides.csv".format(prefix)
 
     with open(output, "w") as unknown_pep_file:
-        filtered_df = peptide_data[np.invert(merged_flag_list)]
+        filtered_df = peptide_data[merged_flag_list]
         filtered_df.to_csv(unknown_pep_file, sep=',', mode='w', header=True,
                            line_terminator='\n')
 
 
-def main():
-    print("started at: " + datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S"))
-    csv_data = extract_csv_data("data/propep_g.csv")
-    database_file = "data/sample_sprot.fasta"
-    cpu = cpu_count()
-    # if database_file.endswith(('fasta', 'fa')):
-    #     with open(database_file, "r") as database:
-    #         flags = search_peptide_db(csv_data, database)
-    # else:
-    #     with gzip.open(database_file, "rt") as database:
-    #         flags = search_peptide_db(csv_data, database)
-    pool = mp.Pool(processes=cpu)
-    results = pool.map(search_peptide_db, [(csv_data, database_file, cpu, i) for i in range(1, cpu + 1)])
-    pool.close()
-    pool.join()
+def main(argv):
+    print(' '.join(argv))
+    print("Started at: " + datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S"))
 
-    merged_flags = merge_flags(results)
-    write_unknown_peptide_data(csv_data, merged_flags)
-    print("finished at: " + datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S"))
+    csv_file = ''
+    database_file = ''
+    output_prefix = ''
+    cpu = os.cpu_count()
+
+    try:
+        opts, args = getopt.getopt(argv[1:], 'c:d:p:', ['csv=', 'database=', 'prefix='])
+    except getopt.GetoptError:
+        print(__doc__)
+        sys.exit(2)
+
+    for opt, arg in opts:
+        if opt in ('-c', '--csv'):
+            csv_file = arg
+        elif opt in ('-d', '--database'):
+            database_file = arg
+        elif opt in ('-p', '--prefix'):
+            output_prefix = arg
+        else:
+            print(__doc__)
+            sys.exit(2)
+
+    try:
+        os.makedirs("output/unknown_peptides")
+    except FileExistsError:
+        pass
+
+    try:
+        csv_data = extract_csv_data(csv_file)
+        # if database_file.endswith(('fasta', 'fa')):
+        #     with open(database_file, "r") as database:
+        #         flags = search_peptide_db(csv_data, database)
+        # else:
+        #     with gzip.open(database_file, "rt") as database:
+        #         flags = search_peptide_db(csv_data, database)
+        pool = mp.Pool(processes=cpu)
+        results = pool.map(search_peptide_db, [(csv_data, database_file, cpu, i) for i in range(1, cpu + 1)])
+        pool.close()
+        pool.join()
+
+        merged_flags = merge_flags(results)
+        write_unknown_peptide_data(csv_data, merged_flags, output_prefix)
+        print("Finished at: " + datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S"))
+    except FileNotFoundError:
+        print(__doc__)
 
 
 if __name__ == '__main__':
-    main()
+    main(sys.argv)
