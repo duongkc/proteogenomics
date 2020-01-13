0f4d22b8c7d4a74b467bc079b47cbd07bae6906f
KC <k.c.duong@st.hanze.nl>
2020-01-13 11:26:06 +0100

    Edit comparison py module to be less static
    The option to specify the names for the separate files has been added
    so this module can be used for comparing files other than just
    between GenemarkS-T and Transdecoder, e.g. between one GenemarkS-T and
    another GenemarkS-T file.

--- peaks_peptide_comparison.py
+++ peaks_peptide_comparison.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
-"""Short module to compare two PEAKS psm search output files to find overlap and distinction between the two in terms
-   of matched peptides.
-
-    usage: peaks_peptide_comparison.py -g <genemark csv file> -t <transdecoder csv file> -p <output prefix>
+"""Short module to compare two PEAKS psm protein-peptide output files to find overlap and distinction between the two
+    in terms of matched peptides. Distinct peptides of each file will be stored in separate files.
 """
 import argparse
 import datetime
@@ -14,21 +12,21 @@ import pandas
 import csv_dataframe
 
 
-def find_distinct_peptides(transdecoder_data, genemark_data, prefix):
+def find_distinct_peptides(left_data, right_data, prefix, left_name, right_name):
     """Filters the CSV files so only distinct peptides remain"""
-    distinct_td_csv = "output/comparison_output/{}_distinct_td.csv".format(prefix)
-    distinct_gm_csv = "output/comparison_output/{}_distinct_gm.csv".format(prefix)
-    with open(distinct_td_csv, "w+") as distinct_transdecoder, \
-            open(distinct_gm_csv, "w+") as distinct_genemark:
-        td_merged = pandas.merge(transdecoder_data, genemark_data, on='Peptide', how='left', indicator=True) \
+    distinct_left_csv = "output/comparison_output/{}_distinct_{}.csv".format(prefix, left_name)
+    distinct_right_csv = "output/comparison_output/{}_distinct_{}.csv".format(prefix, right_name)
+    with open(distinct_left_csv, "w+") as distinct_left, \
+            open(distinct_right_csv, "w+") as distinct_right:
+        left_merged = pandas.merge(left_data, right_data, on='Peptide', how='left', indicator=True) \
             .query("_merge == 'left_only'")
-        gm_merged = pandas.merge(transdecoder_data, genemark_data, on='Peptide', how='right', indicator=True) \
+        right_merged = pandas.merge(left_data, right_data, on='Peptide', how='right', indicator=True) \
             .query("_merge == 'right_only'")
-        td_merged[['Peptide']] \
-            .to_csv(distinct_transdecoder, sep=',', mode='w', index=False, header=['Peptide'],
+        left_merged[['Peptide']] \
+            .to_csv(distinct_left, sep=',', mode='w', index=False, header=['Peptide'],
                     line_terminator='\n')
-        gm_merged[['Peptide']] \
-            .to_csv(distinct_genemark, sep=',', mode='w', index=False, header=['Peptide'],
+        right_merged[['Peptide']] \
+            .to_csv(distinct_right, sep=',', mode='w', index=False, header=['Peptide'],
                     line_terminator='\n')
 
 
@@ -36,12 +34,16 @@ def main(argv):
     print(' '.join(argv))
 
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('-g', '--genemark', action="store", dest="genemark", required=True,
-                        help="Specify the directory of the GenemarkS-T protein-peptides.csv file")
-    parser.add_argument('-t', '--transdecoder', action="store", dest="transdecoder", required=True,
-                        help="Specify the directory of the Transdecoder protein-peptides.csv file")
+    parser.add_argument('-l', '--left', action="store", dest="left",
+                        help="Specify the directory of the first protein-peptides.csv file", required=True)
+    parser.add_argument('-r', '--right', action="store", dest="right",
+                        help="Specify the directory of the second protein-peptides.csv file", required=True)
     parser.add_argument('-p', '--prefix', action="store", dest="prefix", default="sample",
                         help="Provide a prefix for the output csv files")
+    parser.add_argument('--left_name', action="store", dest="left_name", default="left",
+                        help="Name the left sample")
+    parser.add_argument('--right_name', action="store", dest="right_name", default="right",
+                        help="Name the right sample")
     args = parser.parse_args()
 
     try:
@@ -51,10 +53,10 @@ def main(argv):
 
     try:
         print("started at: " + datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S"))
-        genemark_data = csv_dataframe.extract_csv_data(args.genemark)
-        transdecoder_data = csv_dataframe.extract_csv_data(args.transdecoder)
+        left_data = csv_dataframe.extract_csv_data(args.left)
+        right_data = csv_dataframe.extract_csv_data(args.right)
 
-        find_distinct_peptides(transdecoder_data, genemark_data, args.prefix)
+        find_distinct_peptides(left_data, right_data, args.prefix, args.left_name, args.right_name)
         print("finished at: " + datetime.datetime.now().strftime("%d/%m/%Y, %H:%M:%S"))
     except FileNotFoundError:
         print(__doc__)
